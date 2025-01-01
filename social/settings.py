380a3566f04980badbc2cb1bf896d1fb04910f2b
keryo <mohsen.kianmehr88@gmail.com>
2025-01-01 14:02:27 +0330

    customization authenticate

--- social/settings.py
+++ social/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "accounts.authenticate.EmailBackend",
+]
