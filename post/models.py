f74e5af48a051720d7de7ab6431f934ae1b9ddbc
kiankianmehr77 <kiankianmehr7744@gmail.com>
2025-01-02 02:19:49 +0330

    customizetion on admin panel

--- post/models.py
+++ post/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Post (models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     slug = models.SlugField()
     created = models.DateTimeField(auto_now_add=True)
