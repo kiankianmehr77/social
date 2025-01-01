abf20aa5aea43b4bdc8443bc8d2157b450abc962
keryo <mohsen.kianmehr88@gmail.com>
2025-01-02 01:09:48 +0330

    post model

--- social/settings.py
+++ social/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     # my apps:
     'home.apps.HomeConfig',
     'accounts.apps.AccountsConfig',
+    'post.apps.PostConfig'
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
