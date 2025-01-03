47769ddbe3a30b40bcfa55ed2a4a33b9616fa7d8
kiankianmehr77 <kiankianmehr7744@gmail.com>
2025-01-03 20:06:51 +0330

    ordering

--- post/models.py
+++ post/models.py
@@ -9,6 +9,8 @@ class Post (models.Model):
     slug = models.SlugField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-created']
 
     def __str__(self):
         return self.slug
