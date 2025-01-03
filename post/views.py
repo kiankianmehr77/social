ef693505b610528d5e42c2ec1e2889ffff2fd00e
kiankianmehr77 <kiankianmehr7744@gmail.com>
2025-01-04 01:32:14 +0330

    comment

--- post/views.py
+++ post/views.py
@@ -3,15 +3,33 @@ from .models import Post
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from .forms import PostCreateUpdateForm
+from .forms import PostCreateUpdateForm, CommentCreateForm
 from django.utils.text import slugify
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 
 class PostDetailView(View):
-    def get(self, request,post_id, post_slug):
-        post = get_object_or_404(Post, pk=post_id, slug=post_slug)
-        comments = post.post_comments.filter(is_reply = False)
-        return render(request, 'post/detail.html', {'post':post, 'comments' : comments})
+    form_class = CommentCreateForm
+    def setup(self, request, *args, **kwargs):        
+        self.post_instance = get_object_or_404(Post,pk = kwargs['post_id'], slug=kwargs['post_slug'])
+        return super().setup( request, *args, **kwargs)
 
+
+    def get(self, request,*args, **kwargs):
+        post = self.post_instance
+        comments = post.post_comments.filter(is_reply = False)
+        return render(request, 'post/detail.html', {'post':post, 'comments' : comments, 'form' : self.form_class})
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_c=form.save(commit=False)
+            new_c.user = request.user
+            new_c.post = self.post_instance
+            new_c.save()
+            messages.success(request, 'your comment submitted successfully', 'success')
+            return redirect('post:post_detail', self.post_instance.id, self.post_instance.slug)
 
 class PostDeleteView(LoginRequiredMixin,View):
 
