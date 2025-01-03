de992f6501b9ceef3691944bdd277baf7ef4545d
kiankianmehr77 <kiankianmehr7744@gmail.com>
2025-01-03 23:34:49 +0330

    use next for login

--- accounts/views.py
+++ accounts/views.py
@@ -38,6 +38,10 @@ class UserLoginView(View):
     form_class = UserLoginForm
     template_name = 'accounts/login.html'
 
+    def setup(self, request, *args, **kwargs):
+        self.next = request.GET.get('next')
+        return super().setup(request, *args, **kwargs)
+
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             messages.error(request, 'you are already authenticated', 'warning')
@@ -57,6 +61,8 @@ class UserLoginView(View):
             if user is not None:
                 login(request, user)
                 messages.success(request, 'you logged in successfully', 'success')
+                if self.next:
+                    return redirect(self.next)
                 return redirect('home:home')
             messages.error(request, 'username or password is wrong', 'warning')
 
