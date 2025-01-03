2daea0e662294b86943d9bc49fde680e3c42026b
kiankianmehr77 <kiankianmehr7744@gmail.com>
2025-01-03 14:49:08 +0330

    update post

--- post/urls.py
+++ post/urls.py
@@ -5,5 +5,6 @@ app_name = 'post'
 
 urlpatterns = [
     path('<int:post_id>/<slug:post_slug>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('delete/<int:post_id>/', views.PostDeleteView.as_view(), name='post_delete' )
+    path('delete/<int:post_id>/', views.PostDeleteView.as_view(), name='post_delete' ),
+    path('update/<int:post_id>/', views.PostUpdateView.as_view(), name = 'post_update')
 ]
--- post/views.py
+++ post/views.py
@@ -3,6 +3,8 @@ from .models import Post
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from .forms import PostUpdateForm
+from django.utils.text import slugify
 
 class PostDetailView(View):
     def get(self, request,post_id, post_slug):
@@ -21,3 +23,34 @@ class PostDeleteView(LoginRequiredMixin,View):
             messages.error(request, 'you cant delete this post', 'danger')
 
         return redirect('home:home')
+    
+class PostUpdateView(LoginRequiredMixin, View):
+    form_class = PostUpdateForm
+    
+    def setup(self, request, *args, **kwargs):        
+        self.post_instance = Post.objects.get(pk = kwargs['post_id'])
+        return super().setup( request, *args, **kwargs)
+
+
+    def dispatch(self, request, *args, **kwargs):
+        post = self.post_instance
+        if not post.user.id == request.user.id:
+            messages.error(request, 'you cant update this post', 'danger')
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        post = self.post_instance
+
+        form = self.form_class(instance=post)
+        return render(request, 'post/update.html', {'form' : form})
+
+
+    def post(self, request,*args, **kwargs):
+        post = self.post_instance
+        form = self.form_class(request.POST, instance=post)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.save()
+        messages.success(request, 'you updated this post', 'success')
+        return redirect('post:post_detail', post.id, post.slug)
